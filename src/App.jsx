7fd1f50b878ce4595c89b14e3f63c0fb5a087c84
Sem Erik Joop van Velden <vanveldensem@gmail.com>
2024-10-22 16:37:33 +0200

    Initial commit for cv app from template

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
-// import { useState } from 'react'
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import drosoLogo from "./assets/droso.png";
 import "./App.css";
 
-function App() {
-  // const [count, setCount] = useState(0)
+export default function App() {
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://aws.amazon.com" target="_blank">
+          <img src={drosoLogo} className="logo" alt="Droso Logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>CV App</h1>
       <div className="card">
-        {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
-        <p className="hello-world">Hello World!</p>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
-
-export default App;
